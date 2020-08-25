7ec9ed03fe642656ef058ff31254a510f28bb0a5
Tanyiqu <1953649096@qq.com>
2020-08-25 11:05:57 +0800

    准备编写课程管理页面

---
+++ CourseWeb/src/main/webapp/js/adminCourseManage.js
@@ -0,0 +1,9 @@
+/**
+ * 管理员-课程管理菜单相关操作
+ */
+
+// 加载用户页面
+function loadCoursePage() {
+    console.log('切换到课程管理');
+}
+
--- CourseWeb/src/main/webapp/js/adminUserManage.js
+++ CourseWeb/src/main/webapp/js/adminUserManage.js
@@ -14,11 +14,41 @@ let total_page_teacher = 1;
 
 // 入口函数
 $(function () {
-    // 获取第一页数据
-    refreshTeachers();
-
-
+    // 默认加载用户页面
+    // loadUserPage();
 });
+
+// 点击左侧切换
+{
+    // 左侧按钮
+    let change2User = $('#change2User');
+    let change2Course = $('#change2Course');
+    // 面板
+    let userPanel = $('#userPanel');
+    let coursePanel = $('#coursePanel');
+    change2User.off('click');
+    change2User.click(() => {
+        setClass(change2Course, 'inactive', 'active');
+        setClass(change2User, 'active', 'inactive');
+        userPanel.css('display', 'block');
+        coursePanel.css('display', 'none');
+        loadUserPage();
+    });
+
+    change2Course.off('click');
+    change2Course.click(() => {
+        setClass(change2User, 'inactive', 'active');
+        setClass(change2Course, 'active', 'inactive');
+        userPanel.css('display', 'none');
+        coursePanel.css('display', 'block');
+        loadCoursePage();
+    });
+}
+
+// 加载用户页面
+function loadUserPage() {
+    refreshTeachers();
+}
 
 
 // 刷新教师用户表格
@@ -272,30 +302,6 @@ function renderStudentTable(obj) {
 
 // 事件
 function loadEvents() {
-    // 点击左侧切换
-    {
-        // 左侧按钮
-        let change2User = $('#change2User');
-        let change2Course = $('#change2Course');
-        // 面板
-        let userPanel = $('#userPanel');
-        let coursePanel = $('#coursePanel');
-        change2User.off('click');
-        change2User.click(() => {
-            setClass(change2Course, 'inactive', 'active');
-            setClass(change2User, 'active', 'inactive');
-            userPanel.css('display', 'block');
-            coursePanel.css('display', 'none');
-        });
-
-        change2Course.off('click');
-        change2Course.click(() => {
-            setClass(change2User, 'inactive', 'active');
-            setClass(change2Course, 'active', 'inactive');
-            userPanel.css('display', 'none');
-            coursePanel.css('display', 'block');
-        });
-    }
 
     // 用户管理界面 切换教师/学生
     {
