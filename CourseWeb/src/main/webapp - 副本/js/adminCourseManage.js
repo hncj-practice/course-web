e57d52553060a2613b3b4b09c4cced7f32fa0794
Tanyiqu <1953649096@qq.com>
2020-11-21 15:40:32 +0800

    准备整理目录结构

---
+++ CourseWeb/src/main/webapp - 副本/js/adminCourseManage.js
@@ -0,0 +1,242 @@
+/**
+ * 管理员-课程管理菜单相关操作
+ */
+// 入口函数
+$(function () {
+
+});
+
+// 加载用户页面
+function loadCoursePage() {
+    console.log('切换到课程管理');
+    refreshCourses()
+}
+
+// 刷新课程列表
+function refreshCourses() {
+    let url = COURSE_API.FIND;
+    let param = {
+        page: 1,
+        num: 14
+    };
+    let error = () => {
+        toastr.error('查询失败：服务器异常');
+    };
+    setTimeout(() => {
+        // noinspection JSUnresolvedVariable
+
+        my_ajax(url, param, renderCourseTable, error);
+    }, 500);
+}
+
+// 渲染课程列表
+function renderCourseTable(obj) {
+    console.log(obj);
+    // 没有查到数据
+    if (obj.code === 401) {
+        toastr.warning('没有数据');
+        return;
+    }
+    // 处理数据
+    let courseTable = $('#courseTable');
+    let html = '';
+    let cid, cname, tname, status;
+    obj.data.forEach((item) => {
+        cid = item['cid'];
+        cname = item['cname'];
+        tname = item['tname'];
+        status = item['status'];
+
+        // noinspection HtmlUnknownAttribute
+        html += `
+        <tr>
+            <td>{0}</td>
+            <td><input id="courseName{0}" class="course-name" type="text" value="{1}"></td>
+            <td>{2}</td>
+            <td>{3}</td>
+            <td class="options">
+                <span cid="{0}" class="save-course">保存更改</span>
+                <span cid="{0}" class="delete-course">删除</span>
+            </td>
+        </tr>
+        `.format(cid, cname, tname, status);
+
+    });
+
+    courseTable.html(html);
+
+    // 元素生成后添加事件
+    loadCourseEvents();
+
+}
+
+
+// 加载课程管理相关事件
+function loadCourseEvents() {
+
+    // 删除课程
+    {
+        $('.delete-course').off('click');
+        $('.delete-course').click((e) => {
+            let cid = $(e.target).attr('cid');
+            let body = '确定删除课程？<br>课程ID：' + cid;
+            myBootstrapModel('警告', body, '确定', '取消', () => {
+                // 请求API删除
+                let url = COURSE_API.DELETE;
+                // noinspection DuplicatedCode
+                let param = {
+                    adminuser: adminUN,
+                    adminpwd: adminUP,
+                    courseid: cid
+                };
+                let success = (e) => {
+                    // 成功
+                    if (e.code === 200) {
+                        toastr.success(e.message);
+                        refreshCourses();
+                    } else {
+                        toastr.error(e.message);
+                    }
+                };
+                my_ajax(url, param, success);
+            });
+        });
+    }
+
+    // 修改课程名称
+    {
+        $('.save-course').off('click');
+        $('.save-course').click((e) => {
+            let cid = $(e.target).attr('cid');
+            let newName = $('#courseName' + cid).val();
+            let body = '确定改名？<br>新名称：' + newName;
+            myBootstrapModel('警告', body, '确定', '取消', () => {
+                // 请求API修改
+                let url = COURSE_API.UPDATE;
+                // noinspection DuplicatedCode
+                let param = {
+                    user: adminUN,
+                    pwd: adminUP,
+                    courseid: cid,
+                    name: newName
+                };
+                let success = (e) => {
+                    // 成功
+                    if (e.code === 200) {
+                        toastr.success(e.message);
+                    } else {
+                        toastr.error(e.message);
+                    }
+                };
+                my_ajax(url, param, success);
+            });
+        });
+    }
+
+
+    // 新建课程
+    {
+        // 打开按钮
+        $('#newCourse').off('click');
+        $('#newCourse').click(() => {
+            $('.add-new-course').show(250);
+            console.log('打开');
+            $('#courseClassesAdd').text('');
+            // 加载信息
+            // 加载所有教师信息，放在下拉框里面
+            {
+                let url = TEACHER_API.FIND;
+                let param = {};
+                let success = (e) => {
+                    // 成功
+                    if (e.code === 200) {
+                        // 添加到下拉列表
+                        let html = '';
+                        let teacherList = $('#teacherList');
+                        e['data'].forEach((item) => {
+                            html += `<option value="{0}">{1}</option>`.format(item.tno, item.tno + ':' + item.name);
+                        });
+                        teacherList.html(html);
+                    } else {
+                        toastr.error(e.message);
+                    }
+                };
+                my_ajax(url, param, success);
+            }
+            // 加载所有班级信息，放在下拉框里面
+            {
+                let url = CLASS_API.FIND;
+                let param = {};
+                let success = (e) => {
+                    // 成功
+                    if (e.code === 200) {
+                        // 添加到下拉列表
+                        let html = '';
+                        let classesList = $('#classesList');
+                        e['data'].forEach((item) => {
+                            html += `<option value="{0}">{0}</option>`.format(item.classid);
+                        });
+                        classesList.html(html);
+                    } else {
+                        toastr.error(e.message);
+                    }
+                };
+                my_ajax(url, param, success);
+            }
+        });
+        // 关闭按钮
+        $('#closeAddCourseDiv').off('click');
+        $('#closeAddCourseDiv').click(() => {
+            console.log('关闭');
+            $('.add-new-course').hide(250);
+        });
+
+        // 添加班级候选列表
+        $('#addClassToList').off('click');
+        $('#addClassToList').click(() => {
+            console.log('添加了');
+            // 拿到当前选择的班级
+            let classesList = $('#classesList').val();
+            console.log(classesList);
+            // 添加到候补列表
+            $('#courseClassesAdd').text($('#courseClassesAdd').text() + classesList + ';');
+        });
+
+        // 确定新建按钮
+        $('#addCourseEnsure').off('click');
+        $('#addCourseEnsure').click(() => {
+            console.log('确定新建课程');
+
+            let name = $('#courseNameAdd').val();
+            let tno = $('#teacherList').val();
+            let classes = $('#courseClassesAdd').text().split(";");
+            // 删除最后面的空元素
+            classes.splice(classes.length - 1, 1);
+
+            // 调用API新建
+            let url = COURSE_API.ADD;
+            let param = {
+                adminuser: adminUN,
+                adminpwd: adminUP,
+                semester: 1,
+                tno: tno,
+                cname: name,
+                classid: classes,
+                coverimg: 'default',
+                status: '1'
+            };
+            let success = (e) => {
+                // 成功
+                if (e.code === 200) {
+                    toastr.success(e.message);
+                    $('.add-new-course').hide(250);
+                    refreshCourses();
+                } else {
+                    toastr.error(e.message);
+                }
+            };
+            // 新建
+            my_ajax(url, param, success);
+        });
+    }
+}
---
+++ CourseWeb/src/main/webapp - 副本/js/t-question.js
@@ -0,0 +1,381 @@
+// 入口函数
+$(function () {
+    // 加载题目
+    loadQuestions();
+
+    // 加载事件
+    loadEvents();
+});
+
+
+/**
+ * 加载题目
+ * @param type 类型 [1:选择，2:填空，3:判断]，不填则请求所有类型的题目
+ * @param success 加载成功的回调
+ */
+function loadQuestions(type, success) {
+    let url = QUESTION_API.FIND;
+    let param = {
+        chapterid: currCpid,
+        type: type
+    };
+    let list;
+    // 请求所有类型
+    if (!type) {
+        loadQuestions(1);
+        loadQuestions(2);
+        loadQuestions(3, () => {
+            toastr.success('刷新成功')
+        });
+        return;
+    }
+    my_ajax(url, param, (e) => {
+        let questionList = e.data;
+        let html = '';
+        if (e.code === 200) {
+            switch (type) {
+                // 填空
+                case 2:
+                    let num_tk = 1;
+                    list = $('#tkList');
+                    html = '';
+                    questionList.forEach((item) => {
+                        let question = formatQuestion(item['question'], item['panswer'], 2);
+                        html += `
+                        <div questionid="{0}" class="tk-item">
+                            <p class="question-main">{1}</p>
+                            <p class="question-answer">答案：{2}</p>
+                        </div>                  
+                        `.format(item['pid'], num_tk + '、' + question['question'], question['answer']);
+                        num_tk++;
+                    });
+                    list.html(html);
+                    break;
+                // 判断
+                case 3:
+                    let num_pd = 1;
+                    list = $('#pdList');
+                    html = '';
+                    questionList.forEach((item) => {
+                        let question = formatQuestion(item['question'], item['panswer'], 3);
+                        html += `
+                        <div questionid="{0}" class="pd-item">
+                            <p class="question-main">{1}</p>
+                            <p class="question-answer">答案：{2}</p>
+                        </div>
+                        `.format(item['pid'], num_pd + '、' + question['question'], question['answer']);
+                        num_pd++;
+                    });
+                    list.html(html);
+                    break;
+                // 选择
+                case 1:
+                default:
+                    let num_xz = 1;
+                    list = $('#xzList');
+                    html = '';
+                    questionList.forEach((item) => {
+                        // 格式化题干
+                        let question = formatQuestion(item['question'], item['panswer'], 1);
+                        // 动态生成选项
+                        let optHtml = '';
+                        question['opts'].forEach((opt) => {
+                            optHtml += '<p>{0}</p>'.format(opt);
+                        });
+                        // 添加题目
+                        html += `
+                        <div questionid="{0}" class="xz-item">
+                            <p class="question-main">{1}</p>
+                            <div class="question-opts">
+                            {2}
+                            </div>
+                            <p class="question-answer">答案：{3}</p>
+                        </div>
+                        `.format(item['pid'], num_xz + '、' + question['question'], optHtml, question['answer']);
+                        num_xz++;
+                    });
+                    list.html(html);
+                    break;
+            }
+            if (success) {
+                success();
+            }
+        } else {
+            toastr.error(e.message);
+        }
+    });
+}
+
+// 加载事件
+function loadEvents() {
+
+    // 单独添加题目
+    {
+        $('#addQuestion').off('click');
+        $('#addQuestion').click(() => {
+            $('.add-question-div').show(250);
+        });
+
+        // 关闭窗口
+        $('#closeAddQuestion').off('click');
+        $('#closeAddQuestion').click(() => {
+            $('.add-question-div').hide(250);
+        });
+
+        // 切换页面
+        {
+            let btnAddXZ = $('#btnAddXZ');
+            let btnAddTK = $('#btnAddTK');
+            let btnAddPD = $('#btnAddPD');
+            let addXZ = $('#addXZ');
+            let addTK = $('#addTK');
+            let addPD = $('#addPD');
+            btnAddXZ.click(() => {
+                // 显示
+                addXZ.show();
+                btnAddXZ.addClass('active');
+                addTK.hide();
+                btnAddTK.removeClass('active');
+                addPD.hide();
+                btnAddPD.removeClass('active');
+            });
+            btnAddTK.click(() => {
+                // 显示
+                addXZ.hide();
+                btnAddXZ.removeClass('active');
+                addTK.show();
+                btnAddTK.addClass('active');
+                addPD.hide();
+                btnAddPD.removeClass('active');
+            });
+            btnAddPD.click(() => {
+                // 显示
+                addXZ.hide();
+                btnAddXZ.removeClass('active');
+                addTK.hide();
+                btnAddTK.removeClass('active');
+                addPD.show();
+                btnAddPD.addClass('active');
+            });
+        }
+
+        // 点击添加按钮
+        $('#btbAddTM').click(() => {
+            // 判断添加的题型，编号根据接口文档
+            let type;
+            if ($('#btnAddXZ').hasClass('active')) {
+                type = 1;
+            } else if ($('#btnAddTK').hasClass('active')) {
+                type = 2;
+            } else {
+                type = 3;
+            }
+            let question;
+            let answer;
+            switch (type) {
+                case 3:
+                    console.log('添加判断题');
+                    question = $('#questionPD').val();
+                    if (question.trim() === '') {
+                        toastr.error('请输入题目内容');
+                        break;
+                    }
+                    answer = $('#answerPD').val();
+                    console.log(question);
+                    console.log(answer);
+                    // 添加题目
+                    addQuestion({
+                        chapterid: currCpid,
+                        ptype: type,
+                        question: question,
+                        panswer: answer
+                    }, () => {
+                        // 清空输入
+                        $('#questionPD').val('');
+                        toastr.success('添加成功，刷新后显示');
+                    });
+                    break;
+                case 2:
+                    console.log('添加填空题');
+                    question = $('#questionTK').val();
+                    answer = $('#answerTK').val();
+                    if (question.trim() === '') {
+                        toastr.error('请输入题目内容');
+                        break;
+                    }
+                    if (answer.trim() === '') {
+                        toastr.error('请输入答案');
+                        break;
+                    }
+                    addQuestion({
+                        chapterid: currCpid,
+                        ptype: type,
+                        question: question,
+                        panswer: answer
+                    }, () => {
+                        // 清空输入
+                        $('#questionTK').val('');
+                        $('#answerTK').val('');
+                        toastr.success('添加成功，刷新后显示');
+                    });
+                    break;
+                case 1:
+                default: {
+                    console.log('添加选择题');
+                    question = $('#questionXZ').val();
+                    answer = $('#answerXZ').val();
+                    if (question.trim() === '') {
+                        toastr.error('没有题目内容');
+                        break;
+                    }
+                    // 获取4个选项的值
+                    let opts = [];
+                    // 循环ABCD
+                    let arr = ['A', 'B', 'C', 'D'];
+                    try {
+                        arr.forEach(item => {
+
+                            let id = '#opt' + item;
+                            let value = $(id).val();
+                            if (value.trim() === "") {
+                                if (item === 'A') {
+                                    throw new Error('请输入选项A');
+                                }
+                                if (item === 'B') {
+                                    throw new Error('请输入选项B');
+                                }
+                                throw new Error('空');
+                            }
+                            opts.push(item + '、' + value);
+                        });
+                    } catch (e) {
+                        if (e.message !== '空') {
+                            toastr.error(e.message);
+                            break;
+                        }
+                    }
+                    question += '$' + opts.join('$');
+                    console.log(question);
+                    console.log(answer);
+                    // 添加题目
+                    addQuestion({
+                        chapterid: currCpid,
+                        ptype: type,
+                        question: question,
+                        panswer: answer
+                    }, () => {
+                        // 清空输入
+                        $('#questionXZ').val('');
+                        $('#optA').val('');
+                        $('#optB').val('');
+                        $('#optC').val('');
+                        $('#optD').val('');
+                        toastr.success('添加成功，刷新后显示');
+                    });
+                    break
+                }
+
+            }
+
+        });
+    }
+
+    // 导入题目
+    {
+        // 下载导入模板
+        $('#downloadTemplate').attr('href', STATIC.QUESTION_TEMPLATE);
+        // 按钮的点击事件
+        $('#importQuestion').off('click');
+        $('#importQuestion').click(() => {
+            return $('#importQuestionInput').click();
+        });
+        // input file的change事件
+        $('#importQuestionInput').off('change');
+        $('#importQuestionInput').change((e) => {
+            let files = e.target.files;
+            // 解析excel文件
+            resolveXlsx(false, files, (list) => {
+                try {
+                    list.forEach((item, index) => {
+                        console.log(item);
+                        // 判断模板是否正确
+                        if (!(item['answer'] && item['qtype'] && item['question'])) {
+                            throw new Error('请使用正确的模板！');
+                        }
+                        // 请求添加题目API
+                        let param = {
+                            chapterid: currCpid,
+                            ptype: item['qtype'],
+                            question: item['question'],
+                            panswer: item['answer']
+                        };
+                        // 最后一次成功回调后刷新页面
+                        if (index === list.length - 1) {
+                            addQuestion(param, loadQuestions);
+                        } else {
+                            addQuestion(param);
+                        }
+                    });
+                } catch (e) {
+                    toastr.error(e.message);
+                }
+
+            });
+        });
+    }
+}
+
+// 单独添加题目
+function addQuestion(param, success) {
+    let url = QUESTION_API.ADD;
+    param['user'] = teacherId;
+    param['pwd'] = teacherPassword;
+    my_ajax(url, param, (e) => {
+        if (e.code === 200) {
+            if (success) {
+                success();
+            }
+        } else {
+            toastr.error(e.message);
+        }
+    });
+}
+
+
+/**
+ * 格式化题目
+ * @param questionStr   题干字符串
+ * @param answerStr     答案字符串
+ * @param type          题目类型
+ */
+function formatQuestion(questionStr, answerStr, type) {
+    switch (type) {
+        // 填空题
+        case 2:
+            let answer = answerStr.replace(/\$/g, '、');
+            return {
+                question: questionStr,
+                answer: answer
+            };
+        // 判断题
+        case 3:
+            return {
+                question: questionStr,
+                answer: answerStr
+            };
+        // 选择题
+        case 1:
+        default:
+            let questions = questionStr.split('$');
+            let q = questions[0];
+            let opts = [];
+            for (let i = 1; i < questions.length; i++) {
+                opts.push(questions[i])
+            }
+            return {
+                question: q,
+                opts: opts,
+                answer: answerStr
+            };
+    }
+}
