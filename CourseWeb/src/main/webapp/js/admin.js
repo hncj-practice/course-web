13accd3b1274bdef54d3e0b5e8d817818104fb3e
Tanyiqu <1953649096@qq.com>
2020-11-21 17:54:54 +0800

    管理员页面 使用iframe管理页面

--- CourseWeb/src/main/webapp/js/admin.js
+++ CourseWeb/src/main/webapp/js/admin.js
@@ -1,32 +1,29 @@
 // 入口函数
 $(function () {
-    switchTab();
-
+    loadEvents();
 });
 
-// 点击左侧切换
-function switchTab() {
-    // 左侧按钮
-    let change2User = $('#change2User');
-    let change2Course = $('#change2Course');
-    // 面板
-    let userPanel = $('#userPanel');
-    let coursePanel = $('#coursePanel');
-    change2User.off('click');
-    change2User.click(() => {
-        setClass(change2Course, 'inactive', 'active');
-        setClass(change2User, 'active', 'inactive');
-        userPanel.css('display', 'block');
-        coursePanel.css('display', 'none');
-        loadUserPage();
-    });
+/**
+ * 加载事件
+ */
+function loadEvents() {
+    // 左侧菜单切换
+    {
+        let change2User = $('#change2User');
+        let change2Course = $('#change2Course');
 
-    change2Course.off('click');
-    change2Course.click(() => {
-        setClass(change2User, 'inactive', 'active');
-        setClass(change2Course, 'active', 'inactive');
-        userPanel.css('display', 'none');
-        coursePanel.css('display', 'block');
-        loadCoursePage();
-    });
+
+        change2User.click(() => {
+            change2User.addClass('active');
+            change2Course.removeClass('active');
+            $('#mainFrame').attr('src', 'admin-user-manage.html');
+        });
+
+        change2Course.click(() => {
+            change2Course.addClass('active');
+            change2User.removeClass('active');
+            $('#mainFrame').attr('src', 'admin-course-manage.html');
+        });
+
+    }
 }
