c25c88c963e9146306c633cdaaf9e07bdbc57c42
Tanyiqu <1953649096@qq.com>
2020-11-25 12:03:39 +0800

    管理员 分离出实体操作

---
+++ CourseWeb/src/main/webapp/js/entity.js
@@ -0,0 +1,50 @@
+/**
+ * 定义实体类
+ */
+
+/**
+ * 实体
+ */
+const Entity = {
+    TEACHER: 'teacher',
+    STUDENT: 'student',
+    CLASS: 'class'
+};
+
+
+/**
+ * 获取某个实体的所有信息
+ * @param {Entity} entity
+ * @returns {Promise}
+ */
+function getEntities(entity) {
+    let url;
+    switch (entity) {
+        case Entity.TEACHER:
+            url = TEACHER_API.FIND;
+            break;
+        case Entity.STUDENT:
+            url = STUDENT_API.FIND;
+            break;
+        case Entity.CLASS:
+            url = CLASS_API.FIND;
+            break;
+    }
+
+    return new Promise((resolve, reject) => {
+        post(url, {}).then(data => {
+            if (data.code === 200) {
+                resolve(data.data);
+            } else {
+                if (data.message) {
+                    reject(data.message);
+                } else {
+                    reject(ErrorCode["10001"]);
+                }
+            }
+        }).catch(reason => {
+            reject(reason);
+        });
+    });
+
+}
