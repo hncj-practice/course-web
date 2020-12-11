a0a65b6aaa0134387742609d1c5bab099632feaf
Tanyiqu <1953649096@qq.com>
2020-12-11 17:42:29 +0800

    实现教师验证登录

--- course-web/js/teacher.js
+++ course-web/js/teacher.js
@@ -1,53 +1,51 @@
 // 入口函数
 $(function () {
     let sign = getQueryString('sign');
-    let teacherId = localStorage['course-web-curr-teacher-username'];
-    let teacherPwd = localStorage['course-web-curr-teacher-password'];
-
-    // 检验sign
-    if (sign === md5(teacherId + teacherPwd)){
-        console.log(true)
-    }
-
-    // let flag = false;
-
-    // (async function () {
-    //     // 验证登录
-    //     await checkLogin(teacherId, teacherPwd);
-    //
-    //
-    //     // 请求课程信息
-    //     if (flag) {
-    //         await requestCourses();
-    //     }
-    // })();
-
-
+    teacherId = localStorage['course-web-curr-teacher-username'];
+    teacherPwd = localStorage['course-web-curr-teacher-password'];
+    (async function () {
+        // 检验sign
+        if (sign === md5(teacherId + teacherPwd)) {
+            // 检验成功，登录成功
+            console.log(true);
+            // 加载教师的课程
+            await requestCourses(teacherId);
+        } else {
+            // 检验本地存储的是否正确
+            if (!(teacherId && teacherPwd)) {
+                window.location.href = 'user-login.html';
+            }
+            // 检证
+            await checkAndLogin(teacherId, teacherPwd);
+        }
+    })();
 });
 
 
-// 验证登录
-async function checkLogin(teacherId, teacherPwd) {
-    console.log(teacherId);
-    console.log(teacherPwd);
+// 验证并登录
+async function checkAndLogin(teacherId, teacherPwd) {
     let [err, data] = await awaitWrap(post(API.ACCOUNT_API.LOGIN, {
         username: teacherId,
         password: teacherPwd,
         type: 2
     }));
-    console.log([err, data]);
+    if (err) {
+        window.location.href = 'user-login.html';
+    } else {
+        console.log(data);
+        await requestCourses(data.data.tno);
+    }
 }
 
 
 // 请求课程信息
-async function requestCourses() {
+async function requestCourses(id) {
     // 进行请求，请求未完成时，显示加载中
     let url = COURSE_API.FIND_BY;
     let param = {
-        condition: $('#tno').text(),
+        condition: id,
         type: 1
     };
-
     let [err, data] = await awaitWrap(post(url, param));
     if (err) {
         if (isString(err)) {
@@ -60,7 +58,6 @@ async function requestCourses() {
         console.log(data);
         loadCourses(data);
     }
-
 }
 
 
